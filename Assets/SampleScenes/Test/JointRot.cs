c360b7a337a0dd387e483724330d6405e61994bb
striter <letsbattle@foxmail.com>
2020-12-10 20:29:52 +0800

    Update

---
+++ Assets/SampleScenes/Test/JointRot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+public class JointRot : MonoBehaviour
+{
+    ConfigurableJoint joint;
+    public Transform sourceRotation;
+    private void Start()
+    {
+        joint = GetComponent<ConfigurableJoint>();
+    }
+    private void Update()
+    {
+        joint.targetRotation = sourceRotation.localRotation;
+    }
+
+}
