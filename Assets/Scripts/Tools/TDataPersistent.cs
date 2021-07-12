6af205b7180dee41241fa529fef57d985aafe9a7
striter <letsbattle@foxmail.com>
2021-07-12 15:13:55 +0800

    Update TDataPersistent.cs

--- Assets/Scripts/Tools/TDataPersistent.cs
+++ Assets/Scripts/Tools/TDataPersistent.cs
@@ -1,127 +1,119 @@
-﻿using System.Collections.Generic;
-using System.Xml;
+﻿using System.Xml;
 using System.IO;
 using UnityEngine;
 using System.Reflection;
 using System;
-using System.Threading.Tasks;
 
 namespace TDataPersistent
 {
     public class CDataSave<T> where T:class,new()
     {
         public static readonly FieldInfo[] s_FieldInfos = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-        public static readonly string s_FilePath =  "/" + typeof(T).Name + ".xml";
+        public static readonly string s_FilePath =  "/" + typeof(T).Name + ".sav";
         public virtual bool DataCrypt() => true;
-        public virtual void DataRecorrect() { }
     }
     public static class TDataSave
     {
-        public const string m_DataCryptKey = "StriteRTestCrypt";
-        public static readonly string s_Directory = Application.persistentDataPath + "/Save";
-        static XmlNode m_ParentNode;
-        static XmlNode temp_SubNode;
-        public static XmlDocument m_Doc = new XmlDocument();
-        public static string GetFiledPath<T>() where T : CDataSave<T>, new() => s_Directory + CDataSave<T>.s_FilePath;
-        static void Init<T>() where T : CDataSave<T>, new()
-        {
-            try    //Check If File Complete
-            {
-                if (!Directory.Exists(s_Directory))
-                    throw new Exception("None Save Path Found:" + s_Directory);
-
-                string filedPath = GetFiledPath<T>();
-                if (!File.Exists(filedPath))
-                    throw new Exception("None Xml Data Found:" + filedPath);
-
-                m_Doc.Load(filedPath);
-                m_ParentNode = m_Doc.SelectSingleNode(typeof(T).Name);
-                if (m_ParentNode == null)
-                    throw new Exception("None Xml Parent Found:" + typeof(T).Name);
-
-                foreach(var fieldInfo in CDataSave<T>.s_FieldInfos)
-                    if (m_ParentNode.SelectSingleNode(fieldInfo.Name) == null)
-                        throw new Exception("Invalid Xml Child:" + fieldInfo.Name);
-            }
-            catch
-            {
-                CreateDefaultDoc<T>();
-            }
-        }
-
+        private const string c_DataCryptKey = "StriteRTestCrypt";
+        private static readonly string s_persistentPath = Application.persistentDataPath + "/Save";
+        private static readonly string s_streamingPath = Application.streamingAssetsPath + "/Save";
+        private static XmlDocument m_Doc = new XmlDocument();
+        private static string GetStreamingPath<T>() where T : CDataSave<T>, new() => s_streamingPath + CDataSave<T>.s_FilePath;
+        public static string GetPersistentPath<T>() where T : CDataSave<T>, new() => s_persistentPath + CDataSave<T>.s_FilePath;
         public static void ReadPersistentData<T>(this T _data) where T : CDataSave<T>,new ()
         {
-            Init<T>();
             try
             {
-                ReadData(_data);
+                Validate<T>(GetPersistentPath<T>(),out var parentNode);
+                ReadData(_data,parentNode);
             }
-            catch (Exception e)
+            catch (Exception ePersistent)
             {
-                Debug.LogWarning("Data Read Error:" + e.Message);
-                CreateDefaultDoc<T>();
-                ReadData(_data);
+                Debug.LogWarning("Data Read Fail,Use Streaming Data:\n" + ePersistent.Message);
+                ReadDefaultData(_data);
             }
         }
 
-        public static void SavePersistentData<T>(this CDataSave<T> data) where T: CDataSave<T>,new ()
+        public static void SavePersistentData<T>(this CDataSave<T> _data) where T: CDataSave<T>,new ()
         {
-            Init<T>();
+            string persistentPath = GetPersistentPath<T>();
             try
             {
-                SaveData(data);
+                Validate<T>(persistentPath,out var parentNode);
+                SaveData(_data,parentNode,persistentPath);
             }
             catch(Exception e)
             {
-                Debug.LogWarning("Data Save Error:" + e.Message);
-                CreateDefaultDoc<T>();
+                Debug.LogWarning("Data Save Error,Use Streaming Data\n" + e.Message);
+                ReadDefaultData(_data);
             }
         }
 
+        static void Validate<T>(string _path,out XmlNode _node) where T : CDataSave<T>, new()
+        {
+            m_Doc.Load(_path);
+            _node = m_Doc.SelectSingleNode(typeof(T).Name);
+            if (_node == null)
+                throw new Exception("None Xml Parent Found:" + typeof(T).Name);
 
-        static void ReadData<T>(CDataSave<T> _data) where T:CDataSave<T>,new()
+            foreach(var fieldInfo in CDataSave<T>.s_FieldInfos)
+                if (_node.SelectSingleNode(fieldInfo.Name) == null)
+                    throw new Exception("Invalid Xml Child:" + fieldInfo.Name);
+        }
+
+        static void ReadData<T>(CDataSave<T> _data,XmlNode _node) where T:CDataSave<T>,new()
         {
             bool dataCrypt = _data.DataCrypt();
             FieldInfo[] fieldInfo = CDataSave<T>.s_FieldInfos;
             for (int i = 0; i < fieldInfo.Length; i++)
             {
-                string readData = m_ParentNode.SelectSingleNode(fieldInfo[i].Name).InnerText;
-                if (dataCrypt) readData = TDataCrypt.EasyCryptData(readData, m_DataCryptKey);
+                string readData = _node.SelectSingleNode(fieldInfo[i].Name).InnerText;
+                if (dataCrypt) readData = TDataCrypt.EasyCryptData(readData, c_DataCryptKey);
                 fieldInfo[i].SetValue(_data, TDataConvert.Convert(fieldInfo[i].FieldType, readData));
             }
-            _data.DataRecorrect();
         }
         
-        static void SaveData<T>(CDataSave<T> _data) where T : CDataSave<T>, new()
+        static void SaveData<T>(CDataSave<T> _data, XmlNode _node,string _path) where T : CDataSave<T>, new()
         {
             bool dataCrypt = _data.DataCrypt();
             FieldInfo[] fieldInfos = CDataSave<T>.s_FieldInfos;
-            for (int i = 0; i < fieldInfos.Length; i++)
+            foreach (var t in fieldInfos)
             {
-                temp_SubNode = m_ParentNode.SelectSingleNode(fieldInfos[i].Name);
-                string saveData = TDataConvert.Convert(fieldInfos[i].FieldType, fieldInfos[i].GetValue(_data));
-                if (dataCrypt) saveData = TDataCrypt.EasyCryptData(saveData, m_DataCryptKey);
-                temp_SubNode.InnerText = saveData;
-                m_ParentNode.AppendChild(temp_SubNode);
+                var fieldNode = _node.SelectSingleNode(t.Name);
+                string saveData = TDataConvert.Convert(t.FieldType, t.GetValue(_data));
+                if (dataCrypt) saveData = TDataCrypt.EasyCryptData(saveData, c_DataCryptKey);
+                fieldNode.InnerText = saveData;
+                _node.AppendChild(fieldNode);
             }
-            m_Doc.Save(GetFiledPath<T>());
+            m_Doc.Save(_path);
         }
 
-        static void CreateDefaultDoc<T>() where T:CDataSave<T>,new()
+        static void ReadDefaultData<T>(CDataSave<T> _data) where T : CDataSave<T>, new()
         {
-            Debug.LogWarning("Default Data Created:"+typeof(T).Name);
-            if (!Directory.Exists(s_Directory))
-                Directory.CreateDirectory(s_Directory);
+            try
+            {
+                Validate<T>( GetStreamingPath<T>(),out XmlNode node);
+                ReadData(_data,node);
+                SaveData(_data,node,GetPersistentPath<T>());
+            }
+            catch(Exception eStreaming)
+            {
+                Debug.LogError("Streaming Data Invalid,Use BuiltIn-Code:\n"+eStreaming.Message);
 
-            string filePath = GetFiledPath<T>();
-            if (File.Exists(filePath))
-                File.Delete(filePath);
+                if (!Directory.Exists(s_persistentPath))
+                    Directory.CreateDirectory(s_persistentPath);
+        
+                string filePath = GetPersistentPath<T>();
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
 
-            m_Doc = new XmlDocument();
-            m_ParentNode = m_Doc.AppendChild(m_Doc.CreateElement(typeof(T).Name));
-            foreach(var fieldInfo in CDataSave<T>.s_FieldInfos)
-                m_ParentNode.AppendChild(m_Doc.CreateElement(fieldInfo.Name));
-            SaveData(new T());
+                m_Doc = new XmlDocument();
+                var node = m_Doc.AppendChild(m_Doc.CreateElement(typeof(T).Name));
+                foreach(var fieldInfo in CDataSave<T>.s_FieldInfos)
+                    node.AppendChild(m_Doc.CreateElement(fieldInfo.Name));
+            
+                SaveData(new T(),node,GetPersistentPath<T>());
+            }
         }
     }
 
