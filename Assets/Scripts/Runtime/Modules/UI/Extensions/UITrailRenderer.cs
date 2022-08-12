9e928fcbad744eab11daadfb3eafd1aaa273918b
striter <letsbattle@foxmail.com>
2022-08-12 11:02:23 +0800

    UGUI Trail Renderer

--- Assets/Scripts/Runtime/Modules/UI/Extensions/UITrailRenderer.cs
+++ Assets/Scripts/Runtime/Modules/UI/Extensions/UITrailRenderer.cs
@@ -14,9 +14,10 @@ public class UITrailRenderer : MaskableGraphic
     }
     
     public float m_Time = 5f;
-    [Clamp(0f)]public float m_Width = 5f;
+    public AnimationCurve m_Width;
     public float m_MinVertexDistance = .1f;
-
+    public bool m_Emitting = true;
+    
     private float kMinVertexDistanceSqr = 0f;
     private List<TrailPath> m_TrailPaths = new List<TrailPath>();
     private float m_TimeElapsed;
@@ -88,6 +89,9 @@ public class UITrailRenderer : MaskableGraphic
         for (int i = 0; i < dequeueCount; i++)
             m_TrailPaths.RemoveAt(0);
 
+        if (!m_Emitting)
+            return;
+        
         if(m_TrailPaths.Count==0 || (m_TrailPaths[^1].position - transform.position.XY()).sqrMagnitude > kMinVertexDistanceSqr)
             EnqueuePosition();
     }
@@ -101,8 +105,6 @@ public class UITrailRenderer : MaskableGraphic
         var worldToObject = transform.worldToLocalMatrix;
         List<UIVertex> vertices = new List<UIVertex>();
         List<int> indexes = new List<int>();
-
-        var totalLength = 0f;
         
         var count = m_TrailPaths.Count;
         Vector2[] forwardDirections = new Vector2[count - 1];
@@ -114,109 +116,44 @@ public class UITrailRenderer : MaskableGraphic
         var startForward = forwardDirections[0];
         var startUpward = new Vector2(-startForward.y, startForward.x);
         var startPosition = m_TrailPaths[0].position;
-        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint(startPosition+startUpward*m_Width),uv0 = new Vector4(totalLength,0),color = color});
-        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint(startPosition-startUpward*m_Width),uv0 = new Vector4(totalLength,1),color = color});
+        var startWidth = m_Width.Evaluate(1f);
+        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint(startPosition+startUpward*startWidth),uv0 = new Vector4(1,0),color = color});
+        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint(startPosition-startUpward*startWidth),uv0 = new Vector4(1,1),color = color});
         
         for (int i = 1; i < count - 1; i++)
         {
             var curPosition = m_TrailPaths[i].position;
-            var prePosition = m_TrailPaths[i - 1].position;
-
+            var curU = 1f- ((float) i / count);
             var forward0 =  forwardDirections[i-1];
             var forward1 = forwardDirections[i];
             var upward0 = new Vector2(-forward0.y, forward0.x);
             var upward1 = new Vector2(-forward1.y, forward1.x);
-            var clockwise = Vector2.Dot(upward1, forward0) > 0;
+            // var clockwise = Vector2.Dot(upward1, forward0) > 0;
+            var cornerDirection = (upward0 + upward1) / 2 ;
+            cornerDirection *= m_Width.Evaluate((m_TimeElapsed-m_TrailPaths[i].time)/m_Time);
+            var pointT = curPosition + cornerDirection;
+            var pointD = curPosition - cornerDirection;
 
-            if (clockwise)
-            {
-                var corner = (upward0 + upward1).normalized;
+            vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( pointT),uv0 = new Vector4(curU,0),color = color});
+            vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( pointD),uv0 = new Vector4(curU,1),color = color});
+            
+            indexes.Add(curIndex);
+            indexes.Add(curIndex + 2);
+            indexes.Add(curIndex + 1);
 
-                var point4 = curPosition - corner * m_Width;
-                var point2 = point4 + upward0 * m_Width * 2;
-                var point3 = point4 + upward1 * m_Width * 2;
-
-                var length0 = (point4 + upward0 * m_Width - prePosition).magnitude;
-                var length01 = (point4 + upward1 * m_Width - curPosition).magnitude;
-                vertices.Add(new UIVertex()
-                {
-                    position = worldToObject.MultiplyPoint(point2), uv0 = new Vector4(totalLength + length0, 1),
-                    color = color
-                });
-                vertices.Add(new UIVertex()
-                {
-                    position = worldToObject.MultiplyPoint(point3),
-                    uv0 = new Vector4(totalLength + length0 + length01, 1), color = color
-                });
-                vertices.Add(new UIVertex()
-                {
-                    position = worldToObject.MultiplyPoint(point4),
-                    uv0 = new Vector4(totalLength + length0 + length01, 0), color = color
-                });
-                totalLength += length0 + length01;
-
-                indexes.Add(curIndex);
-                indexes.Add(curIndex + 2);
-                indexes.Add(curIndex + 1);
-
-                indexes.Add(curIndex + 2);
-                indexes.Add(curIndex + 4);
-                indexes.Add(curIndex + 1);
-
-                indexes.Add(curIndex + 2);
-                indexes.Add(curIndex + 3);
-                indexes.Add(curIndex + 4);
-            }
-            else
-            {
-                var corner = (upward0 + upward1).normalized;
-                
-                var point3 = curPosition + corner * m_Width;
-                var point2 = point3 - upward0 * m_Width * 2;
-                var point4 = point3 - upward1 * m_Width * 2;
-                
-                var length0 = (point3 - upward0 * m_Width - prePosition).magnitude;
-                var length01 = (point3 - upward1 * m_Width - curPosition).magnitude;
-                vertices.Add(new UIVertex()
-                { 
-                    position = worldToObject.MultiplyPoint(point2), uv0 = new Vector4(totalLength + length0, 0),
-                    color = color
-                });
-                vertices.Add(new UIVertex()
-                {
-                    position = worldToObject.MultiplyPoint(point3),
-                    uv0 = new Vector4(totalLength + length0 + length01, 1), color = color
-                });
-                vertices.Add(new UIVertex()
-                {
-                    position = worldToObject.MultiplyPoint(point4),
-                    uv0 = new Vector4(totalLength + length0 + length01, 0), color = color
-                });
-                totalLength += length0 + length01;
-                
-                indexes.Add(curIndex);
-                indexes.Add(curIndex + 3);
-                indexes.Add(curIndex + 1);
-                
-                indexes.Add(curIndex + 3);
-                indexes.Add(curIndex + 2);
-                indexes.Add(curIndex + 1);
-                
-                indexes.Add(curIndex + 2);
-                indexes.Add(curIndex + 3);
-                indexes.Add(curIndex + 4);
-            }
-            curIndex += 3;
+            indexes.Add(curIndex + 2);
+            indexes.Add(curIndex + 3);
+            indexes.Add(curIndex + 1);
+            
+            curIndex += 2;
         }
 
-        var lastTrail = m_TrailPaths[^1].position;
         var finalPoint = transform.position.XY();
-        var lastDelta =  finalPoint - lastTrail;
-        totalLength += lastDelta.magnitude;
         var lastForwardDirection = forwardDirections[^1];
         var lastUpDirection = new Vector2(-lastForwardDirection.y,lastForwardDirection.x).normalized;
-        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( finalPoint + lastUpDirection * m_Width),uv0 = new Vector4(totalLength,0),color = color});
-        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( finalPoint - lastUpDirection * m_Width),uv0 = new Vector4(totalLength,1),color = color});
+        var lastWidth = m_Width.Evaluate(0f);
+        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( finalPoint + lastUpDirection * lastWidth),uv0 = new Vector4(0,0),color = color});
+        vertices.Add(new UIVertex(){position = worldToObject.MultiplyPoint( finalPoint - lastUpDirection * lastWidth),uv0 = new Vector4(0,1),color = color});
 
         indexes.Add(curIndex);
         indexes.Add(curIndex + 2);
