331a2c090bf8a611ea08d9166c2a592667e2d27c
striter <letsbattle@foxmail.com>
2022-12-30 17:59:11 +0800

    Update

--- Assets/Scenes/Examples/Algorithm/DataStructures/DataStructures.cs
+++ Assets/Scenes/Examples/Algorithm/DataStructures/DataStructures.cs
@@ -22,7 +22,7 @@ namespace Examples.Algorithm.DataStructures
         [ExtendButton("Random",nameof(Randomize))]
         public int m_RandomCount = 128;
         
-        // public KDTree m_KDTree;
+        private KDTree m_KDTree = new KDTree();
         private QuadTree m_Tree = new QuadTree();
 
         void Randomize()
@@ -41,41 +41,51 @@ namespace Examples.Algorithm.DataStructures
         {
             if (m_RandomPoints == null)
                 return;
-            
+
+            Gizmos.matrix = Matrix4x4.identity;            
             m_Tree.Construct(m_Boundary,m_NodeCapacity,m_Iteration);
             m_Tree.Insert(m_RandomPoints);
             m_Tree.DrawGizmos();
-            // m_KDTree.Tick(m_MaxBounds);
-            // m_KDTree.DrawGizmos();
+
+            Gizmos.matrix = Matrix4x4.Translate(Vector3.right * 50f);
+            m_KDTree.Construct(m_Boundary,m_NodeCapacity,m_Iteration);
+            m_KDTree.Insert(m_RandomPoints);
+            m_KDTree.DrawGizmos();
         }
     }
 
-    public class QuadTree
+    public class TreeNode
     {
-        private class QuadTreeNode
-        {
-            public int m_Iteration;
-            public G2Box m_Boundary;
-            public List<float2> m_Points;
-        }
+        public int m_Iteration;
+        public G2Box m_Boundary;
+        public List<float2> m_Points;
+    }
 
+    public abstract class ATree2D
+    {
         private int m_MaxIteration;
         private int m_NodeCapacity;
-        private List<QuadTreeNode> m_Nodes = new List<QuadTreeNode>();
+
+        private List<TreeNode> m_Nodes = new List<TreeNode>();
         public void Construct(G2Box _boundary, int _nodeCapacity,int _maxIteration)
         {
             m_NodeCapacity = _nodeCapacity;
             m_MaxIteration = _maxIteration;
             m_Nodes.Clear();
-            m_Nodes.Add(new QuadTreeNode()
+            m_Nodes.Add(new TreeNode()
             {
                 m_Iteration = 0,
                 m_Boundary = _boundary,
                 m_Points = new List<float2>(),
             });
         }
+        
+        public void Insert(float2[] _points)
+        {
+            foreach (var point in _points)
+                Insert(point);
+        }
 
-        private static G2Box[] kDivides = new G2Box[4];
         public void Insert(float2 _point)
         {
             for (int i = 0; i < m_Nodes.Count; i++)
@@ -91,124 +101,74 @@ namespace Examples.Algorithm.DataStructures
                 if (node.m_Iteration >= m_MaxIteration) //Divide
                     continue;
                
-                
-                var b = node.m_Boundary;
-                var halfExtent = b.extend / 2;
-                var extent = b.size / 2;
-                var min = b.min;
                 var newIteration = node.m_Iteration+1;
-                
-                kDivides[0]= new G2Box(min + extent * new float2(.5f,.5f),halfExtent);
-                kDivides[1]= new G2Box(min + extent * new float2(1.5f,0.5f),halfExtent);
-                kDivides[2]= new G2Box(min + extent * new float2(0.5f,1.5f),halfExtent);
-                kDivides[3]= new G2Box(min + extent * new float2(1.5f,1.5f),halfExtent);
-
-                m_Nodes.RemoveAt(i);
-                m_Nodes.AddRange(kDivides.Select(p=>new QuadTreeNode()
+                m_Nodes.AddRange(Divide(node.m_Boundary,node.m_Iteration).Select(_boundary=>new TreeNode()
                 {
                     m_Iteration = newIteration,
-                    m_Boundary = p,
-                    m_Points = new List<float2>()
+                    m_Boundary = _boundary,
+                    m_Points = node.m_Points.Collect(p=>_boundary.Contains(p)).ToList(),
                 }));
                 
-                foreach (var point in node.m_Points)
-                {
-                    var index = kDivides.FindIndex(p => p.Contains(point));
-                    if(index==-1)
-                        continue;
-                    m_Nodes[i+index].m_Points.Add(point);
-                }
-                
+                m_Nodes.RemoveAt(i);
                 i--;
             }
         }
-        
-        public void Insert(float2[] _points)
-        {
-            foreach (var point in _points)
-                Insert(point);
-        }
 
+        protected abstract G2Box[] Divide(G2Box _src, int _iteration);
+        
         public void DrawGizmos()
         {
             int index = 0;
             foreach (var node in m_Nodes)
             {
                 Gizmos.color = UColor.IndexToColor(index++ % 6);
-                Gizmos.DrawWireCube(node.m_Boundary.center.to3xz(),node.m_Boundary.size.to3xz());
+                Gizmos.DrawWireCube(node.m_Boundary.center.to3xz(),node.m_Boundary.size.to3xz()*.99f);
                 foreach (var point in node.m_Points)
                     Gizmos.DrawWireSphere(point.to3xz(),.1f);
             }
         }
     }
     
-    // public enum EAxis
-    // {
-    //     X,
-    //     Y,
-    //     Z,
-    // }
-    //
-    // [Serializable]
-    // public class KDTree
-    // {
-    //     [Range(0,1)] public float m_Ratio;
-    //     [Clamp(1,20)] public uint iteration = 5;
-    //     
-    //     private List<Node> nodes = new List<Node>();
-    //
-    //     public void Tick(GBox _bounds)
-    //     {
-    //         nodes.Clear();
-    //         EAxis axis = EAxis.X;
-    //         GBox curBounds = _bounds;
-    //         for (int i = 0; i < iteration; i++)
-    //         {
-    //             float3 curSize = 0;
-    //             float3 nextSize = 0;
-    //             float3 nextMove = 0;
-    //             switch (axis)
-    //             {
-    //                 case EAxis.X:
-    //                 {
-    //                     curSize = new float3(m_Ratio, 1, 1);
-    //                     nextSize = new float3(1 - m_Ratio, 1, 1);
-    //                     nextMove = new float3(m_Ratio, 0, 0);
-    //                 }
-    //                     break;
-    //
-    //                 case EAxis.Y:
-    //                 {
-    //                     curSize = new float3(1, m_Ratio, 1);
-    //                     nextSize = new float3(1, 1 - m_Ratio, 1);
-    //                     nextMove = new float3(0,  m_Ratio, 0);
-    //                 }
-    //                     break;
-    //                 case EAxis.Z:
-    //                 {
-    //                     curSize = new float3(1, 1, m_Ratio);
-    //                     nextSize = new float3(1, 1, 1 - m_Ratio);
-    //                     nextMove =  new float3(0, 0, m_Ratio);
-    //                  }
-    //                 break;
-    //             }
-    //
-    //             axis = axis.Next();
-    //             
-    //             nodes.Add(new Node(){bounds = curBounds.Split(0,curSize)});
-    //             curBounds = curBounds.Split(nextMove,nextSize);
-    //         }
-    //         nodes.Add(new Node(){bounds = curBounds});
-    //     }
-    //
-    //     public void DrawGizmos()
-    //     {
-    //         int index = 0;
-    //         foreach (var node in nodes)
-    //         {
-    //             Gizmos.color = UColor.IndexToColor(index++%6);
-    //             node.bounds.DrawGizmos();
-    //         }
-    //     }
-    // }
+    public class QuadTree : ATree2D
+    {
+        private static G2Box[] kDivides = new G2Box[4];
+        protected override G2Box[] Divide(G2Box _src, int _iteration)
+        {
+            var b = _src;
+            var halfExtent = b.extend / 2;
+            var extent = b.size / 2;
+            var min = b.min;
+            
+            kDivides[0]= new G2Box(min + extent * new float2(.5f,.5f),halfExtent);
+            kDivides[1]= new G2Box(min + extent * new float2(1.5f,0.5f),halfExtent);
+            kDivides[2]= new G2Box(min + extent * new float2(0.5f,1.5f),halfExtent);
+            kDivides[3]= new G2Box(min + extent * new float2(1.5f,1.5f),halfExtent);
+            return kDivides;
+        }
+    }
+    
+    public class KDTree : ATree2D
+    {
+        private static G2Box[] kDivides = new G2Box[4];
+        protected override G2Box[] Divide(G2Box _src, int _iteration)
+        {
+            var b = _src;
+            var extent = b.size / 2;
+            var min = b.min;
+            
+            if (_iteration%2 == 1)
+            {
+                var halfExtent = b.extend  * new float2(1,0.5f);
+                kDivides[0]= new G2Box(min + extent * new float2(1f,.5f),halfExtent);
+                kDivides[1]= new G2Box(min + extent * new float2(1f,1.5f),halfExtent);
+            }
+            else
+            {
+                var halfExtent = b.extend * new float2(0.5f,1f);
+                kDivides[0]= new G2Box(min + extent * new float2(.5f,1f),halfExtent);
+                kDivides[1]= new G2Box(min + extent * new float2(1.5f,1f),halfExtent);
+            }
+            return kDivides;
+        }
+    }
 }
