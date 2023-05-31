710d5c4e5da8a5a2fedef02e1184624ce0546013
striter <letsbattle@foxmail.com>
2023-05-31 16:34:26 +0800

    Update

--- Assets/Scripts/Runtime/Math/Polynomial.cs
+++ Assets/Scripts/Runtime/Math/Polynomial.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Mathematics;
+using UnityEngine;
 using static umath;
 using static kmath;
 using static Unity.Mathematics.math;
@@ -49,8 +50,7 @@ public struct QuadraticPolynomial :IPolynomial
 
     public static int GetRoots(float _quadratic, float _linear, float _constant,out float[] _roots)
     {
-        if (_quadratic == 0)
-            return LinearPolynomial.GetRoots(_linear, _constant,out _roots);
+        if (_quadratic == 0) return LinearPolynomial.GetRoots(_linear, _constant,out _roots);
         
         _roots = null;
         var d = _linear * _linear - 4 * _quadratic * _constant;
@@ -72,8 +72,6 @@ public struct QuadraticPolynomial :IPolynomial
     }
     
     public override string ToString() => $"{quadratic}x² {linear.ToStringSigned()}x {constant.ToStringSigned()}";
-
-    
 }
 
 [Serializable]
@@ -90,34 +88,43 @@ public struct CubicPolynomial : IPolynomial
     
     public float Evaluate(float _x) => pow3(_x) * cubic + pow2(_x) * quadratic + _x * linear + constant;
 
-
     public static int GetRoots(float _cubic,float _quadratic, float _linear, float _constant, out float[] _roots)
     {
-        if (_cubic == 0)
-            return QuadraticPolynomial.GetRoots(_quadratic,_linear, _constant, out _roots);
+        if (_cubic == 0) return QuadraticPolynomial.GetRoots(_quadratic,_linear, _constant, out _roots);
         
         var t = _cubic;
-        var a = _quadratic;
-        var b = _linear;
-        var c = _constant;
+        
+        var a = _quadratic / t;
+        var b = _linear / t;
+        var c = _constant / t;
+
+        var cMinus = a / 3; //roots = (x - a/3)
+        
         var p = -kInv9 * pow2(a) + kInv3 * b;
         var q = (1f / 27) * pow3(a) - kInv6 * a * b +  kInv2 * c;
         var D = -(pow3(p) + pow2(q));
 
         var sqrtNegD = sqrt(-D);
         var r = pow(-q + sqrtNegD  , kInv3);
-
+        var s = pow(abs(-q - sqrtNegD)  , kInv3);
         if (D == 0)
         {
-            _roots = new[] {r, -r};
+            _roots = new[] { 2*r - cMinus, -r - cMinus};
             return 2;
         }
 
-        var omega = kInv3 * pow(cos(-q / sqrt(-pow3(p))), -1);
-        var x2 = 2 * sqrt(-p) * cos(omega + kPI2 / 3);
-        var x3 = 2 * sqrt(-p) * cos(omega - kPI2 / 3);
-        
-        _roots = new []{r,x2,x3};
+        if (D < 0)     //To be continued 
+        {
+            _roots = new[] { r+s - cMinus};
+            return 1;
+        }
+
+        var omega = kInv3 * acos(-q / sqrt(-pow3(p)));
+        var sqrtNegP = sqrt(-p);
+        var x1 = 2 * sqrtNegP * cos(omega);
+        var x2 = 2 * sqrtNegP * cos(omega + kPI2 / 3);
+        var x3 = 2 * sqrtNegP * cos(omega - kPI2 / 3);
+        _roots = new []{ x1 -cMinus,x2 - cMinus,x3 - cMinus};
         return 3;
     }
 
